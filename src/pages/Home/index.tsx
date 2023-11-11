c18765f9842363c48ef33b9f0c64495a2fbf8531
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2023-11-12 01:39:15 +0900

    chore: add prettierrc, vite absolute path and tsconfig

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import Test from '@src/common/components/Test';
+
+const index = () => {
+  return (
+    <div>
+      index
+      <Test />
+    </div>
+  );
+};
+
+export default index;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@public', replacement: resolve(__dirname, 'public') },
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      {
+        find: '@common',
+        replacement: resolve(__dirname, 'src/common'),
+      },
+      {
+        find: '@styles',
+        replacement: resolve(__dirname, 'src/styles'),
+      },
+      {
+        find: '@pages',
+        replacement: resolve(__dirname, 'src/pages'),
+      },
+    ],
+  },
+  plugins: [react(), viteTsconfigPaths()],
+});
