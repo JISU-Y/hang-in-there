8fb0f37fa31438eea5c13924a4c8b4f3f8026e71
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2023-11-19 01:28:13 +0900

    style: structuring nav and footer

--- src/common/layouts/PageLayout.tsx
+++ src/common/layouts/PageLayout.tsx
@@ -1,6 +1,21 @@
 import { PropsWithChildren } from 'react';
 import styled from '@emotion/styled';
 
+const footerData: { listTitle: string; label: string }[] = [
+  {
+    listTitle: '사이드 프로젝트 소개',
+    label: `행사를 즐기는 인싸들 여기서 만나자!\n힘든 일상을 버티게 해줄 행사 플랫폼, HANG IN THERE!`
+  },
+  {
+    listTitle: '기획자/PM',
+    label: '유지아 | dbwldk7675@gmail.com'
+  },
+  {
+    listTitle: '개발자',
+    label: '유지수 | jisu129@gmail.com'
+  }
+];
+
 interface PageLayoutProps {
   withNavbar?: boolean;
   withFooter?: boolean;
@@ -13,9 +28,18 @@ const PageLayout = ({
 }: PropsWithChildren<PageLayoutProps>) => {
   return (
     <PageContainer>
-      {withNavbar && <Navbar>행인들 Navbar</Navbar>}
+      {withNavbar && <Navbar>행인들(아이콘)</Navbar>}
       {children}
-      {withFooter && <footer>행인들 footer</footer>}
+      {withFooter && (
+        <Footer>
+          {footerData.map(({ listTitle, label }) => (
+            <div>
+              <span>{listTitle}</span>
+              <span>{label}</span>
+            </div>
+          ))}
+        </Footer>
+      )}
     </PageContainer>
   );
 };
@@ -30,6 +54,29 @@ const Navbar = styled.nav`
   width: 100%;
   height: 48px;
   background-color: red;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 36px 42px;
+  background-color: aquamarine;
+
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    & > span:first-child {
+      font-weight: 600;
+    }
+
+    & > span:last-child {
+      text-align: right;
+      white-space: pre-line;
+    }
+  }
 `;
 
 export default PageLayout;
