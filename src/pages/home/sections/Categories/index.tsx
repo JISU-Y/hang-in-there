79081aa33068abc69527ee9a130b8292dca434a6
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2023-11-21 00:39:01 +0900

    feat: category buttons at home

--- src/pages/home/sections/Categories/index.tsx
+++ src/pages/home/sections/Categories/index.tsx
@@ -1,7 +1,38 @@
 import styled from '@emotion/styled';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button
+} from '@chakra-ui/react';
+
+// 축제 공연 전시 교육/체험 아동/청소년
+const categoryList = {
+  축제: null,
+  공연: ['연극', '뮤지컬', '노래/춤'],
+  전시: null,
+  '교육/체험': null,
+  '아동/청소년': ['청소년 축제', '청소년 공연', '청소년 교육/체험']
+};
 
 const Categories = () => {
-  return <Container>Categories</Container>;
+  return (
+    <Container>
+      {Object.entries(categoryList).map(([key, value]) => (
+        <Menu>
+          <MenuButton as={Button}>{key}</MenuButton>
+          <MenuList>
+            {value?.map(subMenu => <MenuItem>{subMenu}</MenuItem>)}
+          </MenuList>
+        </Menu>
+      ))}
+    </Container>
+  );
 };
 
 const Container = styled.section`
