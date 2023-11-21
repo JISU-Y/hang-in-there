c5f8ca41ff2953ac561d0baf8be3beb4a018b695
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2023-11-22 00:02:56 +0900

    feat: collection banner ui and add carousel

--- src/pages/home/sections/Collections/index.tsx
+++ src/pages/home/sections/Collections/index.tsx
@@ -1,13 +1,53 @@
 import styled from '@emotion/styled';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './custom-slick.css';
+
+const settings = {
+  dots: true,
+  dotsClass: 'custom-dots',
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
 
 const Collections = () => {
-  return <Container>collecitons</Container>;
+  return (
+    <Container>
+      <Slider {...settings}>
+        <ImageWrapper>
+          <Image src={`/logo/intro.png`} alt="intro-logo" />
+        </ImageWrapper>
+        {[1, 2, 3, 4, 5, 6, 7].map(festival => (
+          <ImageWrapper key={festival}>
+            <Image
+              src={`/temp/festival-${festival}.png`}
+              alt={`festival-${festival}`}
+            />
+          </ImageWrapper>
+        ))}
+      </Slider>
+    </Container>
+  );
 };
 
 const Container = styled.section`
   width: 100%;
   height: 360px;
-  background-color: aqua;
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 360px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 
 export default Collections;
