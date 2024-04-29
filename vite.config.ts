68c483fa7701685692611e53ed28d1ccdbe5d4ba
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2024-04-29 23:19:43 +0900

    fix: api proxy vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default ({ mode }) => {
   // 현재 작업 디렉터리의 `mode`를 기반으로 env 파일을 불러옴
   // 세 번째 매개변수를 ''로 설정하면 `VITE_` 접두사에 관계없이 모든 환경 변수를 불러옴
   const env = loadEnv(mode, process.cwd(), '');
-  console.log(env.VITE_TOUR_API_END_POINT);
 
   return defineConfig({
     resolve: {
@@ -29,18 +28,18 @@ export default ({ mode }) => {
       ]
     },
     plugins: [react(), viteTsconfigPaths()],
-    // NOTE: 임시로 proxy production에도 설정
     server: {
       // Proxy 설정
       proxy: {
         // 경로가 "/api" 로 시작하는 요청을 대상으로 proxy 설정
         '/api': {
           // 요청 전달 대상 서버 주소 설정
-          target: 'http://59.7.183.217:3000', // env.VITE_TOUR_API_END_POINT,
+          target: env.VITE_HANGINTHERE_API_END_POINT,
           // 요청 헤더 host 필드 값을 대상 서버의 호스트 이름으로  변경
           changeOrigin: true,
           // 요청 경로에서 '/api' 제거
-          rewrite: path => path.replace(/^\/api/, ''),
+          rewrite: path =>
+            path.replace(/^\/api/, env.VITE_HANGINTHERE_API_END_POINT),
           // SSL 인증서 검증 무시
           secure: false
         }
