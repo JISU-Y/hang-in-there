4e127b6f29a6bc2cbafe13ca7468c33cfe144ccc
유지수 <yoojisu@yujisuui-MacBookAir.local>
2024-07-28 22:47:42 +0900

    remove: netlify toml

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 
 const nextConfig = {
   images: {
-    unoptimized: isDevelopment
+    unoptimized: isDevelopment,
+    domains: ['tong.visitkorea.or.kr']
   },
   experimental: {
     missingSuspenseWithCSRBailout: false
