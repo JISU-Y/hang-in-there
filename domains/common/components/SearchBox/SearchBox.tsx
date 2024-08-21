fab68663c8b479cde89a149227b05b6ceb834c8d
JISU YOO <80020227+JISU-Y@users.noreply.github.com>
2024-08-22 00:59:39 +0900

    Feat: search input (#33)
    * feat: popular events dropdown
    
    * feat: search result list
    
    * feat: highlight keyword in search result
    
    * feat: popular event list api
    
    * feat: search result list
    
    * feat: route to event detail from search box
    
    * remove: search type file
    
    ---------
    
    Co-authored-by: 유지수 <yoojisu@yujisuui-MacBookAir.local>

--- domains/common/components/SearchBox/SearchBox.tsx
+++ domains/common/components/SearchBox/SearchBox.tsx
@@ -2,6 +2,7 @@ import {
   ChangeEventHandler,
   FormEventHandler,
   useEffect,
+  useRef,
   useState
 } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
@@ -12,14 +13,52 @@ import {
   categories,
   CategoryCodeType
 } from '@domains/common/constants/categories';
+import { useBooleanState } from '@toss/react';
+import { useOutsideClick } from '@chakra-ui/react';
+import {
+  useFetchPopularEventListQuery,
+  useFetchSearchEventResultQuery
+} from '@domains/common/network/searchQueries';
+
+const getHighlightedText = (
+  text: string,
+  highlight: string,
+  highlightColor?: string
+) => {
+  const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+  return (
+    <span>
+      {parts.map((part, i) => (
+        <span
+          key={i}
+          style={
+            part.toLowerCase() === highlight.toLowerCase()
+              ? { color: highlightColor || 'red' }
+              : {}
+          }
+        >
+          {part}
+        </span>
+      ))}
+    </span>
+  );
+};
 
 const SearchBox = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
   const { push } = useRouter();
   const searchParams = useSearchParams();
   const categoryCode = searchParams.get('category') as CategoryCodeType;
   const searchKeyword = searchParams.get('search');
 
   const [keyword, setKeyword] = useState('');
+
+  const [isOpenDropdown, openDropdown, closeDropdown] = useBooleanState();
+
+  const { data: popularEventList } = useFetchPopularEventListQuery();
+  const { data: searchResultList } = useFetchSearchEventResultQuery(keyword); // TODO: debounce 추가 필요
 
   const handleChangeKeyword: ChangeEventHandler<HTMLInputElement> = e => {
     setKeyword(e.target.value);
@@ -37,12 +76,23 @@ const SearchBox = () => {
     push(`/category?search=${keyword}`);
   };
 
+  const goToEventDetail = (eventId: number) => {
+    push(`/eventDetail/${eventId}`);
+
+    setKeyword('');
+  };
+
   useEffect(() => {
     setKeyword(searchKeyword || '');
   }, [searchKeyword, categoryCode]);
 
+  useOutsideClick({
+    ref: containerRef,
+    handler: closeDropdown
+  });
+
   return (
-    <Container>
+    <Container ref={containerRef} onFocus={openDropdown}>
       <InputForm onSubmit={handleSubmitSearch}>
         <SearchIcon width="24px" height="24px" />
         <Input
@@ -51,6 +101,46 @@ const SearchBox = () => {
           placeholder="원하시는 행사를 검색해보세요."
         />
       </InputForm>
+      {isOpenDropdown && (
+        <SearchDropdown ref={dropdownRef}>
+          <ResultListWrapper>
+            {keyword ? (
+              <SearchResultContainer>
+                {searchResultList &&
+                  searchResultList.map(result => (
+                    <SearchResultEvent
+                      key={result.event_id}
+                      onClick={() => goToEventDetail(result.event_id)}
+                    >
+                      {getHighlightedText(result.title, keyword, '#FF6917')}
+                    </SearchResultEvent>
+                  ))}
+              </SearchResultContainer>
+            ) : (
+              <>
+                <PopularTitle>인기행사</PopularTitle>
+                {popularEventList &&
+                  popularEventList.map(event => (
+                    <PopularCard
+                      key={event.event_id}
+                      onClick={() => goToEventDetail(event.event_id)}
+                    >
+                      <RankNumber>{event.rank}</RankNumber>
+                      <EventInfo>
+                        <EventTitle>{event.title}</EventTitle>
+                        <EventAddress>
+                          {event.address
+                            ? event.address.split(' ').slice(0, 2).join(' ')
+                            : '미정'}
+                        </EventAddress>
+                      </EventInfo>
+                    </PopularCard>
+                  ))}
+              </>
+            )}
+          </ResultListWrapper>
+        </SearchDropdown>
+      )}
     </Container>
   );
 };
@@ -81,6 +171,99 @@ const Input = styled.input`
 
   &::placeholder {
     color: #adadad;
+  }
+`;
+
+const SearchDropdown = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+  max-height: 424px;
+  padding: 24px 0;
+  background-color: #ffffff;
+  border-bottom-left-radius: 12px;
+  border-bottom-right-radius: 12px;
+  border: 1px solid #ededed;
+  z-index: 10;
+`;
+
+const ResultListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const PopularTitle = styled.h4`
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  padding: 0 32px;
+`;
+
+const PopularCard = styled.div`
+  display: flex;
+  gap: 8px;
+  width: 100%;
+  height: 56px;
+  cursor: pointer;
+  padding: 0 32px;
+
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #ededed;
+  }
+`;
+
+const RankNumber = styled.span`
+  font-size: 36px;
+  font-weight: 700;
+  line-height: 56px;
+  color: #ff6917;
+  width: 38px;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+const EventInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const EventTitle = styled.span`
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+`;
+
+const EventAddress = styled.span`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+`;
+
+const SearchResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const SearchResultEvent = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  cursor: pointer;
+  padding: 0 32px;
+
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #ededed;
   }
 `;
 
