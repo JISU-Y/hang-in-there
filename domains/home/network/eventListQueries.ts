3371f9045b44e0022b587eee148be86fcad60f77
유지수 <yoojisu@yujisuui-MacBookAir.local>
2024-10-12 09:21:12 +0900

    chore: upgrade tanstack query

--- domains/home/network/eventListQueries.ts
+++ domains/home/network/eventListQueries.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 
 import { UseQueryOptionsType } from '@domains/common/types/utilType';
 import { EventListResponseDtoNew } from '@domains/category/types';
