43d848991e6cdc5472ce14486650ac6e59592115
유지수 <yoojisu@yujisuui-MacBookAir.local>
2024-12-14 11:20:35 +0900

    fix: add image domains

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,8 @@ const nextConfig = {
       'tong.visitkorea.or.kr',
       'k.kakaocdn.net',
       't1.kakaocdn.net',
+      'hid-banner.s3.ap-northeast-2.amazonaws.com',
+      'hid-event.s3.ap-northeast-2.amazonaws.com',
       'hid-profile.s3.ap-northeast-2.amazonaws.com'
     ]
   },
