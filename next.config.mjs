b0a83740f8da9bf587cd960a5cce4c487dadf108
유지수 <yoojisu@yujisuui-MacBookAir.local>
2025-06-03 18:18:57 +0900

    fix: add image domain

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,8 @@ const nextConfig = {
       't1.kakaocdn.net',
       'hid-banner.s3.ap-northeast-2.amazonaws.com',
       'hid-event.s3.ap-northeast-2.amazonaws.com',
-      'hid-profile.s3.ap-northeast-2.amazonaws.com'
+      'hid-profile.s3.ap-northeast-2.amazonaws.com',
+      'picsum.photos'
     ]
   },
   experimental: {
